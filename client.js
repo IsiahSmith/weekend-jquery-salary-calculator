8a5d609115f6ddfe0098ea12a49a2ced4820ca50
Isiah Smith <isiahthomassmith@gmail.com>
2021-10-02 12:58:51 -0500

    Added readyNow and addEmployee functions

--- client.js
+++ client.js
@@ -1,5 +1,26 @@
+console.log('JS');
+let employeeList = [];
 $(readyNow);
 
 function readyNow() {
+    console.log('JQ');
+    $(`#Submit`).on(`click`, addEmployee)
+};
+
+function addEmployee() {
+    let employeeInfo = {
+        firstName: $(`#firstNameEnter`).val(),
+        lastName: $(`#lastNameEnter`).val(),
+        iD: $(`#idEnter`).val(),
+        title: $(`#titleEnter`).val(),
+        annualSalary: $(`#annualSalaryEnter`).val()
+    };
+    employeeList.push(employeeInfo);
+    console.log(employeeList);
+    
+    render();
+};
+
+function render() {
     
 };
